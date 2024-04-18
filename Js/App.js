8667472f40f93635d570996d01a77d6b112da2eb
MyFrondEndProjects <MyBqadresst1@outlook.com>
2024-04-18 15:34:11 +0300

    Son güncelleme

--- Js/App.js
+++ Js/App.js
@@ -5,34 +5,68 @@ const Hbutton = document.querySelector("#Hbutton");
 const Hakkimizda = document.querySelector(".Hakkimizda");
 const content = document.querySelector(".content");
 const ybutton = document.querySelector("#ybutton");
+const yetkinlikler = document.querySelector(".yetkinlikler");
+const iletisim = document.querySelector(".iletisim");
+const ibutton = document.querySelector("#ibutton");
+
+
+
+
+
+
 
 
 homepage.remove();
 Hakkimizda.remove();
+yetkinlikler.remove();
+iletisim.remove();
+
+
+
+
+
 
 
 const homepageadd = () => {
+
     content.textContent = "";
     content.appendChild(homepage);
+
 }
 
 const hakkimizda = () => {
+
     content.textContent = "";
     content.appendChild(Hakkimizda);
 
 }
 
-Hbutton.addEventListener("click", hakkimizda)
-homebutton.addEventListener("click", homepageadd);
+
 
 
 
 const Yetkinlikler = () => {
+
     content.textContent = "";
+    content.appendChild(yetkinlikler);
+
+}
+
+
+
+const iFun = () => {
+
+    content.textContent = "";
+    content.appendChild(iletisim);
 
 }
 
 
 
 
-ybutton.addEventListener("click", Yetkinlikler)
+
+
+Hbutton.addEventListener("click", hakkimizda);
+homebutton.addEventListener("click", homepageadd);
+ybutton.addEventListener("click", Yetkinlikler);
+ibutton.addEventListener("click", iFun);
